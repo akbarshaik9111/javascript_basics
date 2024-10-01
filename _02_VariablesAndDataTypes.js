759cbba1a2956071b034037f3c33b5a9efe843a6
akbar <akbarshaik9111@gmail.com>
2024-10-01 22:15:11 +0530

    Variables and DataTypes

---
+++ _02_VariablesAndDataTypes.js
@@ -0,0 +1,11 @@
+
+let a = 20;
+let b = 240.6
+let c = "Akbar Shaik";
+let d = true;
+
+// To print variable values and type of variable
+console.log(a+" : "+typeof(a));
+console.log(b+" : "+typeof(b));
+console.log(c+" : "+typeof(c));
+console.log(d+" : "+typeof(d));
